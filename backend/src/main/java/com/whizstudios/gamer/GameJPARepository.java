65061cfac536ce0af420dc713dde3c99d70729dd
Isaac_Whiz <ssekajjawavamuno@gmail.com>
2024-01-30 20:48:40 +0300

    Created a git workflow file and updated postrgres to 15.4

--- backend/src/main/java/com/whizstudios/gamer/GameJPARepository.java
+++
@@ -1,8 +0,0 @@
-package com.whizstudios.gamer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameJPARepository extends JpaRepository<Gamer, Long> {
-    boolean existsGamerByEmail(String email);
-    boolean existsById(long id);
-}
