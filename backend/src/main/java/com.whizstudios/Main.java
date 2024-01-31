8ecef00ddcfddb54166116825975ca7a7571ea17
Isaac_Whiz <ssekajjawavamuno@gmail.com>
2024-01-31 09:59:47 +0300

    Added a comment in the main method

--- backend/src/main/java/com.whizstudios/Main.java
+++ backend/src/main/java/com.whizstudios/Main.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        //This is the application entry point
 
 
     }
