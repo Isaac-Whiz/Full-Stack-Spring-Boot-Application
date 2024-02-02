b8c25662983a26709289fd5fe3b2a29eade34fec
Isaac_Whiz <ssekajjawavamuno@gmail.com>
2024-02-02 06:41:05 +0300

    CD update and a new rest controller

---
+++ backend/src/main/java/com.whizstudios/PingPong.java
@@ -0,0 +1,15 @@
+package com.whizstudios;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPong {
+
+    record Ping(String ping){};
+
+    @GetMapping("/ping")
+    public Ping getPingPong() {
+        return new Ping("Pong");
+    }
+}
