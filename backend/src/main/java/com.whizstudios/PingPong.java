7dc441d1863a0801ca3cca4e931fabb8fdf3a80b
Isaac_Whiz <ssekajjawavamuno@gmail.com>
2024-02-06 05:06:39 +0300

    Added the CD workflow and Pingpoing class

--- backend/src/main/java/com.whizstudios/PingPong.java
+++ backend/src/main/java/com.whizstudios/PingPong.java
@@ -6,11 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingPong {
 
-<<<<<<< HEAD
-    record Ping(String result){};
-=======
     record Ping(String ping){};
->>>>>>> b8c25662983a26709289fd5fe3b2a29eade34fec
 
     @GetMapping("/ping")
     public Ping getPingPong() {
