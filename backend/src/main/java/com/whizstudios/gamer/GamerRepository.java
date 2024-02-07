2a9490a035d66e4c4913bbbec7cf6d8cee70afe1
Isaac_Whiz <ssekajjawavamuno@gmail.com>
2024-02-07 16:08:56 +0300

    Updated GamerRepository and backend-cd

--- backend/src/main/java/com/whizstudios/gamer/GamerRepository.java
+++ backend/src/main/java/com/whizstudios/gamer/GamerRepository.java
@@ -3,12 +3,12 @@ package com.whizstudios.gamer;
 import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GamerRepository extends JpaRepository<Gamer, Long> {
 
     @Nonnull
     Optional<Gamer> findById(Long id);
-
-
+    List<Gamer> findTopBy();
 }
