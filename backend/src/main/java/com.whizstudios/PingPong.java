af8b6089709f1bc5c3b1842ee654233dc8cd2d34
Isaac_Whiz <ssekajjawavamuno@gmail.com>
2024-02-09 04:45:06 +0300

    Added slack emogis and counter to Ping class

--- backend/src/main/java/com.whizstudios/PingPong.java
+++ backend/src/main/java/com.whizstudios/PingPong.java
@@ -8,8 +8,10 @@ public class PingPong {
 
     record Ping(String ping){};
 
+    private static int COUNTER = 0;
+
     @GetMapping("/ping")
     public Ping getPingPong() {
-        return new Ping("Pong successful");
+        return new Ping("Pong successful: " + ++COUNTER + " times");
     }
 }
